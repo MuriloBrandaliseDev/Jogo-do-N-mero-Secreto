3bf1508c7aa83af5cddf1a9f8981bfe8e07be524
MuriloDev <murilobrandalise98@gmail.com>
2024-07-25 15:47:32 -0300

    Mudança no estilo

--- app.js
+++ app.js
@@ -61,5 +61,5 @@ function reiniciarJogo() {
     limparCampo();
     tentativas = 1;
     exibirMensagemInicial();
-    document.getElementById('reiniciar').setAttribute('disabled',true);
+    document.getElementById('reiniciar').setAttribute(true);
 }

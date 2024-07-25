c1194ef1b7365bb74795c528b5760a5820d08f77
MuriloDev <murilobrandalise98@gmail.com>
2024-07-25 14:52:35 -0300

    Remove ignored files from tracking

---
+++ Downloads/Jogo_numero_secreto-main (1)/Jogo_numero_secreto-main/newFile.js
@@ -0,0 +1,13 @@
+if (chute == numeroSecreto) {
+    exibirTextoNaTela('h1', 'Acertou!');
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativas = `Você descobriu o número secreto com  ${tentativas} ${palavraTentativa}!`;
+    exibirTextoNatela('p', mensagemTentativas);
+} else {
+    if (chute > numeroSecreto) {
+        exibirTextoNaTela('p', 'O número secreto é menor');
+    } else {
+        exibirTextoNaTela('p', 'O número secreto é maior');
+    }
+    tentativas++;
+}
